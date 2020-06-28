0dc33458a938a1fda8767f79571b33a94b2f1428
EMENGH <eamendez62@gmail.com>
2020-06-28 13:41:18 -0400

    Create app.js

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,115 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+d3.csv("assets/data/data.csv").then(function(smokeAgeData) {
+
+    console.log(smokeAgeData);
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    smokeAgeData.forEach(function(data) {
+        data.smokes = +data.smokes;
+        data.age = +data.age;
+    });
+  
+      // Step 2: Create scale functions
+      // ==============================
+      var xLinearScale = d3.scaleLinear()
+        .domain([51, d3.max(smokeAgeData, d => d.smokes)])
+        .range([0, chartWidth]);
+  
+      var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(smokeAgeData, d => d.age)])
+        .range([chartHeight, 0]);
+  
+      // Step 3: Create axis functions
+      // ==============================
+      var bottomAxis = d3.axisBottom(xLinearScale);
+      var leftAxis = d3.axisLeft(yLinearScale);
+  
+      // Step 4: Append Axes to the chart
+      // ==============================
+      chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+  
+      chartGroup.append("g")
+        .call(leftAxis);
+  
+      // Step 5: Create Circles
+      // ==============================
+      var circlesGroup = chartGroup.selectAll("circle")
+      .data(smokeAgeData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.smokes))
+      .attr("cy", d => yLinearScale(d.age))
+      .attr("r", "15")
+      .attr("fill", "pink")
+      .attr("opacity", ".5");
+  
+      // Step 6: Initialize tool tip
+      // ==============================
+      var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+          return (`${d.rockband}<br>Hair length: ${d.smokes}<br>Hits: ${d.age}`);
+        });
+  
+      // Step 7: Create tooltip in the chart
+      // ==============================
+      chartGroup.call(toolTip);
+  
+      // Step 8: Create event listeners to display and hide the tooltip
+      // ==============================
+      circlesGroup.on("click", function(data) {
+        toolTip.show(data, this);
+      })
+        // onmouseout event
+        .on("mouseout", function(data, index) {
+          toolTip.hide(data);
+        });
+  
+      // Create axes labels
+      chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Number of Billboard 100 Hits");
+  
+      chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Hair Metal Band Hair Length (inches)");
+    }).catch(function(error) {
+      console.log(error);
+});
+    
